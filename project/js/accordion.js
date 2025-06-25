a33a8a9e7be923f06c75712474029cd09bba46cf
Shimaya Nobuhiko <shimayanobuhiko@gmail.com>
2025-06-25 17:27:42 +0900

    feat: accordionアニメーション追加

--- project/js/accordion.js
+++ project/js/accordion.js
@@ -1,7 +1,16 @@
 function toggleAccordion(clickedElem) {
-  const content = clickedElem.nextElementSibling; // クリックされた要素の次の兄弟要素（問題文など）
-  const arrow = clickedElem.querySelector(".arrow"); // クリックされた要素内の矢印部分
+  const content = clickedElem.nextElementSibling;
+  const arrow = clickedElem.querySelector(".arrow");
 
-  content.classList.toggle("open");
-  arrow.classList.toggle("rotate");
+  const isOpen = content.classList.contains("open");
+
+  if (isOpen) {
+    content.style.maxHeight = 0;
+    content.classList.remove("open");
+    arrow.classList.remove("rotate");
+  } else {
+    content.classList.add("open");
+    content.style.maxHeight = content.scrollHeight + "px";
+    arrow.classList.add("rotate");
+  }
 }
